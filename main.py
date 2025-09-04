1e87bdd74c48bbbd59831ac38faba4e56e874c9c
sclarerot <sclarerot@proton.me>
2025-09-04 17:07:23 -0400

    Implement separate encrypt function
    Made the encryption process it's own function so it could be
    hypothetically reused with greater ease.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-def main() -> None:
+def encrypt(string: str, num: int) -> str:
     alphabet: str = "abcdefghijklmnopqrstuvwxyz"
     upper_alphabet: str = alphabet.upper()
     encrypted_str: str = ""
+
+    for c in string:
+        if c in alphabet:
+            if (alphabet.find(c) + num) < 26:
+                encrypted_str += alphabet[alphabet.find(c) + num]
+            else:
+                encrypted_str += alphabet[(alphabet.find(c) + num) - 26]
+        elif c in upper_alphabet:
+            if (upper_alphabet.find(c) + num) < 26:
+                encrypted_str += upper_alphabet[upper_alphabet.find(c) + num]
+            else:
+                encrypted_str += upper_alphabet[(upper_alphabet.find(c) + num) - 26]
+        else:
+            encrypted_str += c
+
+    return encrypted_str
+
+def main() -> None:
 
     user_str: str = input("Enter the string you want to encrypt: ")
 
@@ -13,21 +31,9 @@ def main() -> None:
         else:
             print("Please enter a valid integer between 1 and 26.")
 
-    for c in user_str:
-        if c in alphabet:
-            if (alphabet.find(c) + user_num) < 26:
-                encrypted_str += alphabet[alphabet.find(c) + user_num]
-            else:
-                encrypted_str += alphabet[(alphabet.find(c) + user_num) - 26]
-        elif c in upper_alphabet:
-            if (upper_alphabet.find(c) + user_num) < 26:
-                encrypted_str += upper_alphabet[upper_alphabet.find(c) + user_num]
-            else:
-                encrypted_str += upper_alphabet[(upper_alphabet.find(c) + user_num) - 26]
-        else:
-            encrypted_str += c
+        secret_str: str = encrypt(user_str, user_num)
 
-    print(f"Here's your secret message: {encrypted_str}")
+    print(f"Here's your secret message: {secret_str}")
 
 if __name__ == "__main__":
     main()
