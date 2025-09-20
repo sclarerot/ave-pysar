ab2a5cb27ca3ce5c0014aed8cb45c4122654b510
sclarerot <sclarerot@proton.me>
2025-09-20 09:44:14 -0400

    Add decrypt functionality and menu

--- main.py
+++ main.py
@@ -19,21 +19,40 @@ def encrypt(string: str, num: int) -> str:
 
     return encrypted_str
 
+def decrypt(string: str) -> str:
+    for i in range(1, 26):
+        print(f"key {i}: {encrypt(string, i)}")
+
 def main() -> None:
+    running: bool = True
+    while running:
+        print("1. Encrypt a string")
+        print("2. Decrypt a string")
+        print("3. Exit")
+        user_choice: str = input("Enter the action you'd like to perform(1, 2, 3): ")
 
-    user_str: str = input("Enter the string you want to encrypt: ")
+        match user_choice:
+            case "1":
+                user_str: str = input("Enter the string you want to encrypt: ")
+                asking: bool = True
+                
+                while asking:
+                    user_num = int(input("Enter the number you want to encrypt your string by: "))
+                    if user_num > 0 and user_num < 27:
+                        asking = False
+                        secret_str: str = encrypt(user_str, user_num)
 
-    asking: bool = True
-    while asking:
-        user_num = int(input("Enter the number you want to encrypt your string by: "))
-        if user_num > 0 and user_num < 27:
-            asking = False
-        else:
-            print("Please enter a valid integer between 1 and 26.")
-
-    secret_str: str = encrypt(user_str, user_num)
-
-    print(f"Here's your secret message: {secret_str}")
+                        print(f"Here's your secret message: {secret_str}")
+                    else:
+                        print("Please enter a valid integer between 1 and 26.")
+            case "2":
+                user_str = input("Please enter the string you would like to decrypt: ")
+                print("Here are all possible decryption keys")
+                decrypt(user_str)
+            case "3":
+                running = False
+            case _:
+                print("Please enter a valid input (1, 2, 3).")
 
 if __name__ == "__main__":
     main()
